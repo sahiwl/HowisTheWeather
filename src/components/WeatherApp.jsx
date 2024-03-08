81a2e017db6203858f8a7322b115c1c25662f3a3
Sahil Kumar Ray <piyushkrray1@gmail.com>
2024-03-08 19:20:49 +0530

    update WeatherApp.jsx

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -4,27 +4,47 @@ import axios from "axios";
 const WeatherApp = () => {
     const [city, setCity] = useState("");
     const [weatherInfo, setWeatherInfo] = useState(null); 
+    const apiKey = import.meta.env.VITE_API_KEY
 
-const fetchApiData = async () => {
+    const fetchApiData = async () => {
+        try {
+          const res = await axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+          );
+          setWeatherInfo(res.data);
+          console.log(res.data); //weather data
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    
+    const inputHandler = async(e) => {
+    setCity(e.target.value);
+  };
 
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=NEWDELHI&units=metric&appid=${apiKey}`
-    )
-    .then(response=>response.json())
-    .then(data=>setWeatherInfo(data))
-    console.log(weatherData); // weather data
+  const submitHandler = (e) => {
+    e.preventDefault();
+    fetchApiData(city);
+  };  
 
-};
-
-    return(
-        <div>
-            <form>
-        <input type="text" placeholder="Enter a city" />
+  return (
+    <div>
+    <form onSubmit={submitHandler}>
+    <input type="text" placeholder="Enter a city" value={city}  onChange={inputHandler}/>
         <button type="submit">Get Weather</button>
-      </form>
-
-        </div>
-    )
-}
+    </form>
+      {weatherInfo ? (
+        <>
+          <h2>{weatherInfo.name}</h2>
+          <p>{weatherInfo.main.temp}°C</p>
+          <p>{weatherInfo.weather[0].description}</p>
+          <p>feels like{weatherInfo.main.feels_like}°C</p>
+          <p>Humidity {weatherInfo.main.humidity}%</p>
+        </>) : (
+        <p>Loading..</p>
+      )}
+    </div>
+  );
+};
 
 export default WeatherApp;
